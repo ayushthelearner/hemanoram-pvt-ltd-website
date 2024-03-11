bb67040f35eb88744112e0bf0e5a57b8ff1c96db
ayushthelearner <ayushrob4@gmail.com>
2024-03-11 18:33:25 +0530

    adding files

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const ejsMate = require("ejs-mate");
+const path = require("path");
+const ejs = require("ejs");
+
+
+
+
+app.engine('ejs',ejsMate);
+app.set("view engine",'ejs');
+app.use(express.static(path.join(__dirname,"/public")));
+app.set("views",path.join(__dirname,"views"));
+
+
+app.listen(3000,()=>{
+    console.log("server is started");
+})
+
+app.get("/home",(req,res)=>{
+    res.render("listings/home.ejs");
+})
+
+app.get("/whatwedo",(req,res)=>{
+    res.render("listings/whatwedo.ejs");
+})
+
+app.get("/rangeofservices",(req,res)=>{
+    res.render("listings/rangeofservices.ejs")
+})
+app.get("/contactus",(req,res)=>{
+    res.render("listings/contactus.ejs")
+})
